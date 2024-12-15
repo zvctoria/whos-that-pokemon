c222671aecd0fd7cd8f787deec7328c96f0965a7
victoria <z5480947@student.unsw.edu.au>
2024-12-15 22:28:01 +1100

    Added queryClient setup and handled non-existent routes

---
+++ src/app/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <>This is not a valid route!</>;
+};
+
+export default NotFound;
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,28 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useMemo } from "react";
+import { QueryClient, useQueryClient } from "@tanstack/react-query";
 import { paths } from "./config/paths";
 
 // renames Default export/property of given module to expected "Component"
-const convert = (mod: any) => {
+const convert = (queryClient: QueryClient) => (mod: any) => {
   // if clientLoader or clientAction is defined within the imported module,
   // invoke with the queryClient argument for shared resourses. Else, undefined
-  const { default: Component, ...rest } = mod;
+  const { clientLoader, clientAction, default: Component, ...rest } = mod;
   return {
     ...rest,
+    loader: clientLoader?.(queryClient),
+    action: clientAction?.(queryClient),
     Component,
   };
 };
 
 // implicit return
-export const createAppRouter = () =>
+export const createAppRouter = (queryClient: QueryClient) =>
   createBrowserRouter([
     {
       path: paths.landing.path,
-      lazy: () => import("./pages/Landing").then(convert),
+      lazy: () => import("./pages/Landing").then(convert(queryClient)),
+    },
+    {
+      path: "*",
+      lazy: () => import("./pages/NotFound").then(convert(queryClient)),
     },
   ]);
 
 export const AppRouter = () => {
-  const router = createAppRouter();
+  const queryClient = useQueryClient();
+
+  const router = useMemo(() => createAppRouter(queryClient), [queryClient]);
 
   return <RouterProvider router={router} />;
 };
