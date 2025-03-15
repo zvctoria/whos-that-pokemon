e4218f268fcc568f217e3f68166ce52f29c7e408
victoria <z5480947@student.unsw.edu.au>
2025-03-16 02:26:52 +1100

    Added basic website structure, no styling or javascript yet

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -1,5 +1,41 @@
 const Landing = () => {
-  return <>This is the landing page!</>;
+  return (
+    <>
+      <div>
+        <h1>Who's that Pokémon?</h1>
+      </div>
+      <div id="select-gens">
+        <div>Gens 1-5</div>
+        <div>Gens 6-9</div>
+      </div>
+      <div>
+        <h2>Please click at least one generation! (Toggle)</h2>
+      </div>
+      <div>
+        <button>Replay Cry</button>
+      </div>
+      <div id="answer-form">
+        <input type="text" />
+      </div>
+      <div>Type () () </div>
+      <div>This Pokémon weighs BOLD(number kg) </div>
+      <div>
+        <p>Not Helpful? MASTER BALL</p>
+        <button>Hint: see what's inside</button>
+      </div>
+      <div>First appeared in: </div>
+      <div>
+        <h2>Can potentially have the following abilities:</h2>
+        <div>
+          <h3>Ability 1 (dynamic)</h3>
+          <p>Description</p>
+        </div>
+      </div>
+      <div>Sprite</div>
+      <button>(when done) Try Again</button>
+      <div>LEFT: settings. RIGHT: pokeball</div>
+    </>
+  );
 };
 
 export default Landing;
