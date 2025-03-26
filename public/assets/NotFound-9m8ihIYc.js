8d043eb7cb6d7e59390ccfc9f0a263a14ea054da
victoria <z5480947@student.unsw.edu.au>
2025-03-27 01:41:54 +1100

    npm run build output

---
+++ public/assets/NotFound-9m8ihIYc.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C3CE_X-1.js";const r=()=>t.jsx(t.Fragment,{children:"This is not a valid route!"});export{r as default};
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "public",
+    outDir: "dist",
   },
 });
